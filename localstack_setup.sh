0ee187f3204131290cb92b7906e132d810188422
Elon <elon@Elons-iMac.local>
2020-03-26 16:16:27 -0700

    receive from sqs

--- localstack_setup.sh
+++ localstack_setup.sh
@@ -1,14 +1,10 @@
 # queue commands
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name tdm-mythos-labs-load.fifo  --attributes "FifoQueue=true"
+aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name movie-load.fifo  --attributes "FifoQueue=true"
 aws sqs list-queues --endpoint-url=http://localhost:4576
 
 # create local table and list it
 aws --endpoint-url=http://localhost:4569 dynamodb create-table --table-name movie-job-information --attribute-definitions AttributeName=job_id,AttributeType=S --key-schema AttributeName=job_id,KeyType=HASH --billing-mode PAY_PER_REQUEST &> /dev/null
 aws --endpoint-url=http://localhost:4569 dynamodb list-tables
-
-
-
-
 
 
 # copy a file named movies.json to the local directory just to see what was generated (from the script through the queue and into the bucket)
@@ -20,4 +16,4 @@ aws --endpoint-url=http://localhost:4569 dynamodb list-tables
 # aws dynamodb scan --table-name movie-job-information --endpoint-url=http://localhost:4569 > table_scan.json
 
 # receives messages from the specified queue (aka reads queue contents)
-# aws sqs receive-message --queue-url http://localhost:4576/queue/load.fifo --endpoint-url=http://localhost:4576 --max-number-of-messages 10
+# aws sqs receive-message --queue-url http://localhost:4576/queue/movie-load.fifo --endpoint-url=http://localhost:4576 --max-number-of-messages 10
--- main.py
+++ main.py
@@ -1,13 +1,59 @@
 import boto3
 import uuid
+import json
+from faker import Faker
+fake = Faker()
+
+sqs = boto3.resource('sqs', region_name='us-west-2', endpoint_url="http://localhost:4576")
+sqs_client = boto3.client('sqs', region_name="us-west-2", endpoint_url="http://localhost:4576")
+queue = sqs.get_queue_by_name(QueueName='movie-load.fifo')
+print(queue.url)
 
 
-def generate_movies()
-    print('generated')
+def generate_movies(movie_count):
+    movies_payload = []
+
+    for counter in range(movie_count):
+        movie = {
+            "title": f"{fake.first_name()} Loves {fake.first_name()}",
+            "year": 2999,
+            "director": fake.name(),
+            "actor": fake.name()
+        }
+
+        movies_payload.append(movie)
+    return movies_payload
+
+
+def enqueue_message(job_id, message_body):
+    queue_url = queue.url
+    sqs_client.send_message(QueueUrl=queue_url,
+                            MessageBody=message_body,
+                            MessageGroupId=job_id,
+                            MessageDeduplicationId=str(uuid.uuid4()))
+
 
 def main():
 
+    # generate
+    job_id = str(uuid.uuid4())
+    movies_payloads = generate_movies(1)
 
+    # send message to queue
+    for movies_payload in movies_payloads:
+        enqueue_message(job_id, json.dumps(movies_payload, sort_keys=True, indent=4))
+
+    # receive message
+    response = sqs_client.receive_message(
+        QueueUrl=queue.url,
+        MaxNumberOfMessages=10,
+        VisibilityTimeout=10,
+        WaitTimeSeconds=10,
+    )
+
+    messages = response.get("Messages")
+
+    print(messages)
 
 if __name__ == '__main__':
     main()
