0d3491ec2e573d3b9901a7c1765bae1fb3a65f9e
Elon Arbiture <elon.arbiture@nike.com>
2020-03-27 16:08:25 -0700

    add localstack commands

--- localstack_setup.sh
+++ localstack_setup.sh
@@ -1,11 +1,14 @@
 # queue commands
 aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name movie-load.fifo --attributes "FifoQueue=true"
-aws sqs list-queues --endpoint-url=http://localhost:4576
+# aws sqs list-queues --endpoint-url=http://localhost:4576
 
 # create local table and list it
 aws --endpoint-url=http://localhost:4569 dynamodb create-table --table-name movie-job-information --attribute-definitions AttributeName=job_id,AttributeType=S --key-schema AttributeName=job_id,KeyType=HASH --billing-mode PAY_PER_REQUEST &> /dev/null
 aws --endpoint-url=http://localhost:4569 dynamodb list-tables
 
+# create bucket
+aws --endpoint-url=http://localhost:4572 s3 mb s3://movie-bucket
+# aws -- endpoint-url=http://localhost:4572 s3 ls
 
 # copy a file named movies.json to the local directory just to see what was generated (from the script through the queue and into the bucket)
 # aws s3 mv s3://movie-bucket/movies.json . --endpoint-url=http://localhost:4572
